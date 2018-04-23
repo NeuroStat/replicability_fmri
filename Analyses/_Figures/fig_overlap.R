9561a088adfe4240223a4c6a97c8508f90bc4522
HBossier <bossier.han@gmail.com>
2018-04-23 09:48:02 +0200

    Removing code on previous sampling scheme

--- Analyses/_Figures/fig_overlap.R
+++ Analyses/_Figures/fig_overlap.R
@@ -50,8 +50,9 @@ library(NeuRRoStat)
 # Dimension of the brains
 DIM <- c(53,63,46)
 
-# Number of runs (so far)
-NRUNS <- 42
+# Number of runs
+NRUNS <- 50
+
 # Number of steps
 NSTEP <- 70
 
@@ -65,6 +66,14 @@ NSTEP <- 70
 MatrixOverlap <- readRDS(paste(LocIntRes,'/MaitraOverlap.rda',sep=''))
 PercAct <- readRDS(paste(LocIntRes,'/PercActMaitraOverlap.rda',sep=''))
 
+## Prepare matrix
+Overlap.tmp <- matrix(MatrixOverlap,ncol=1)
+sampleSize <- rep(seq(10,700,by=10), NRUNS)
+Overlap <- data.frame('overlap' = Overlap.tmp, 'size' = sampleSize)
+
+# Variables for plotting
+subjBreak <- c(seq(0,100,by=20),seq(100,700,by=50))
+
 
 ##
 ###############
@@ -72,70 +81,21 @@ PercAct <- readRDS(paste(LocIntRes,'/PercActMaitraOverlap.rda',sep=''))
 ###############
 ##
 
+# Set window 
 quartz.options(width=18,height=12)
 
-
-## Prepare matrix
-Overlap.tmp <- matrix(MatrixOverlap,ncol=1)
-sampleSize <- rep(seq(10,700,by=10),NRUNS)
-Overlap <- data.frame('overlap' = Overlap.tmp, 'size' = sampleSize)
-
-# Variables for plotting
-subjBreak <- c(seq(0,100,by=20),seq(100,700,by=50))
-
 #################
-## ggplot: points
+## Points for overlap with smoothed regression line
 #################
-ggplot(Overlap, aes(x=factor(sampleSize),y=overlap)) + 
-  geom_point() +
-  scale_x_discrete(breaks=subjBreak, name="Sample size")
-
-
-# Check with earlier results: load in, rename and reload this version
-load("/Volumes/2_TB_WD_Elements_10B8_Han/PhD/IMAGENDATA/Data/FreddieFreeloader/Script.git/Analyses/MatrixOverlap")
-MatrixOverlapComb <- MatrixOverlap
-# Reload results from subjectsplit
-load(paste(wd,'/MatrixOverlapSplitSubjA',sep=''))
-
-
-# Prepare the data, previous data had larger sample size, so we get rid of these
-# Then we add the sample sizes
-# we make a data frame and bind the data from this version to it (with splitted subjects)
-# We add a variable with 1 for this version and 2 for previous version
-MatrixOverlapCombn <- MatrixOverlapComb[,-c(71:74)]
-MatrixOverlapCombn <- t(MatrixOverlapCombn)
-ArrayComb <- matrix(MatrixOverlapCombn,ncol=1)
-combSampleSize <- rep(seq(10,700,by=10),dim(MatrixOverlapCombn)[2])
-combOverlap <- data.frame('overlap' = ArrayComb, 'size' = combSampleSize)
-
-BothOverlap <- rbind(Overlap,combOverlap)
-originData <- c(rep(2,length(sampleSize)),rep(1,length(combSampleSize)))
-BothOverlap <- data.frame(BothOverlap, 'origin' = originData)
-BothOverlap$origin <- factor(BothOverlap$origin)
-
-
-
-## ggplot2
-ggplot(BothOverlap, aes(x=factor(size),y=overlap)) + 
-  geom_point(aes(colour=origin, alpha=origin),position='identity') +
-  scale_x_discrete(breaks=subjBreak, name="Sample size") +
+ggplot(Overlap, aes(x=sampleSize, y = overlap)) + 
+  geom_point(size = 0.6) +
+  geom_smooth(aes(x = sampleSize, y = overlap),
+          method = 'loess', 
+          formula = y ~ x,
+          colour = '#fc8d59') +
+  scale_x_continuous(breaks=subjBreak, name="Sample size") +
   scale_y_continuous(name='Overlap') +
-  scale_alpha_discrete(name='Restrictions', labels = c('No restriction (prev)','No overlapping subjects (curr)')) +
-  scale_colour_discrete(name='Restrictions', labels = c('No restriction (prev)','No overlapping subjects (curr)')) +
-  theme(plot.title = element_text(lineheight=.2, face="bold")) +
-  ggtitle('Comparing previous with current sampling frame')
-
-
-
-#################
-## ggplot: boxplot
-#################
-ggplot(BothOverlap, aes(x=factor(size),y=overlap))+ 
-  geom_boxplot(aes(color=origin),outlier.size = 1,lwd=.9) + 
-  scale_color_manual(values = c('#1f78b4', '#b2df8a'),name="Restrictions", labels=c('None', 'No overlapping subjects')) +
-  ggtitle('Comparing previous with current sampling frame') +
-  theme(plot.title = element_text(lineheight=.2, face="bold")) +
-  scale_x_discrete(breaks=subjBreak, name="Sample size")
+  theme_bw()
 
 
 
@@ -157,36 +117,27 @@ ggplot(obs, aes(x=factor(size), y=count))+
   theme_bw()
 
 
-
-
-
-
-
 #################
 ## plot the percentage of masked voxels being activated
 #################
-
 
 # Data frame with overlap and percentage
 perc <- matrix(PercAct,ncol=1)
 OverPerc <- data.frame('Value' = rbind(Overlap.tmp,perc),
                        'Size' = rep(sampleSize,2), 
-                       'Type' = c(rep('Overlap',2940),rep('Percentage',2940)))
+                       'Type' = c(rep('Overlap',3500),rep('Percentage',3500)))
 OverPerc$Type <- as.factor(OverPerc$Type)
 OverPerc$Value <- as.numeric(OverPerc$Value)
 
 # Correlation
-corr <- round(cor(na.omit(perc),na.omit(Overlap.tmp)),2)
-
+corr <- round(cor(perc, Overlap.tmp, use = "complete.obs"),2)
 ggplot(OverPerc, aes(x=factor(Size),y=Value)) + 
   geom_point(aes(colour=Type),position='identity',size=1.5) +
   scale_x_discrete(breaks=subjBreak, name="Sample size") +
   scale_y_continuous(name='Overlap') +
   theme(plot.title = element_text(lineheight=.2, face="bold")) +
   ggtitle('Overlap and percentage of masked voxels active.') +
-  annotate("text", y = .1, x = 60, label = paste('Correlation = ', corr,sep=''),size=5)
-
-
+  annotate("text", y = .1, x = 50, label = paste('Correlation = ', corr,sep=''),size=5)
 
 # Calculate averages
 tmp <- aggregate(OverPerc,by=list(OverPerc$Size,OverPerc$Type),mean,na.rm=TRUE)
@@ -200,5 +151,5 @@ ggplot(AvgOverPerc, aes(x=factor(Size),y=Value,group=Type)) +
   scale_y_continuous(name='Overlap') +
   theme(plot.title = element_text(lineheight=.2, face="bold")) +
   ggtitle('Average overlap and percentage of masked voxels active.')+
-  annotate("text", y = .1, x = 60, label = paste('Correlation = ', corr,sep=''),size=5)
+  annotate("text", y = .1, x = 50, label = paste('Correlation = ', corr,sep=''),size=5)
 
