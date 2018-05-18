54d334282d09e367e63282861c988a2db60d7bd1
HBossier <bossier.han@gmail.com>
2018-05-18 17:04:13 +0200

    working example of EM, need to clean up, try with different starting values and add stopping rule!

--- Sampling/Coherence/EM_mix_binom.R
+++ Sampling/Coherence/EM_mix_binom.R
@@ -24,11 +24,14 @@
 ###############
 ##
 
+# Libraries
+library(dplyr)
+
 # Number of steps in algorithm for testing purpose
 nstep <- 100
 
 # Number of voxels for testing purpose
-nvox <- 100
+nvox <- 100000
 
 # Number of independent maps (R) for testing purpose
 R <- 10
@@ -50,20 +53,24 @@ weight <- function(lambda, pi_A1, pi_I1, R, Gi){
 # Maximalization function that returns updated 
 #   value for pi_A1, given current values (_c)
 update_pi_A <- function(lambda_c, pi_A1_c, pi_I1_c, R, Gi){
+  # Weight of current step
+  weight_c <- weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi)
   # First calculate numinator
-  num <- sum((weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi) * Gi))
+  num <- sum(weight_c * Gi)
   # Now calculate denominator
-  denom <- sum((weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi) * R))
+  denom <- sum(weight_c * R)
   # Return value
   return(num/denom)
 }
 
 # Same for pi_I1
 update_pi_I <- function(lambda_c, pi_A1_c, pi_I1_c, R, Gi){
+  # Weight of current step
+  weight_c <- weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi)
   # First calculate numinator
-  num <- sum(((1 - weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi)) * Gi))
+  num <- sum(((1 - weight_c) * Gi))
   # Now calculate denominator
-  denom <- sum(((1 - weight(lambda_c, pi_A1_c, pi_I1_c, R, Gi = Gi)) * R))
+  denom <- sum(((1 - weight_c) * R))
   # Return value
   return(num/denom)
 }
@@ -80,80 +87,173 @@ update_lambda <- function(lambda_c, pi_A1_c, pi_I1_c, R, Gi){
 ###############
 ##
 
-# Test weight function
-weight(0.8,0.2,0.7,8,1:8)
-
 # Create test data
 set.seed(pi*3.14)
 
-# No coherence if sampled at random (?)
-TDat_R <- sample(x = 1:R, size = nvox, replace = TRUE)
+## Data as mixture of binomials
+true_PIA1 <- 0.75
+true_PII1 <- 0.2
+true_lambda <- 0.25
 
-# many null voxels, some random and some coherence (partial coherence)
-TDat_PC <- c(rep(0, 60), sample(x = 1:R, size = 20, replace = TRUE), rep(9,20))
+# Small simulation 
+nsim <- 100
 
-# data with only 0 and R (full coherence)
-TDat_FC <- sample(c(0, R), size = nvox, replace = TRUE)
+sim_PIA <- sim_PII <- sim_lambda <- c()
 
-# Data with only R
-TDat_FR <- rep(R, nvox)
-
-# Data with only 0
-TDat_F0 <- rep(0, nvox)
-
-# Start for loop of algorithm steps
-for(v in 1:nstep){
-  ################################
-  ## 1: Initial starting values ##
-  ################################
-  if(v == 1){
-    lambda <- 0.5
-    pi_A1 <- 0.1
-    pi_I1 <- 0.1
+for(i in 1:nsim){
+  # Create data
+  # First generate the mixing parameter (Z)
+  TDat_mix <- data.frame(Z = rbinom(n = nvox, size = 1, prob = true_lambda)) %>%
+    # Mutate column with data, depending on mixing parameter
+    mutate(G = ifelse(Z == 1,
+              rbinom(n = 1, size = R, prob = true_PIA1),
+              rbinom(n = 1, size = R, prob = true_PII1)))
+  
+  # Start for loop of algorithm steps
+  for(v in 1:nstep){
+    ################################
+    ## 1: Initial starting values ##
+    ################################
+    if(v == 1){
+      lambda <- true_lambda
+      pi_A1 <- true_PIA1
+      pi_I1 <- true_PII1
+      #cat( "Iteration: 0", "| PI A1 =", pi_A1, "| PI I0 =", pi_I0, "| Lambda =", lambda,  "\n" )
+    }
+  
+    ################################
+    ## 2: Conditional expectation ##
+    ################################
+    
+    # See custom functions and derivations in PDF
+  
+    #####################################
+    ## 3: Maximise (update parameters) ##
+    #####################################
+    
+    # Parameter: pi_A1
+    pi_A_up <- update_pi_A(lambda, pi_A1, pi_I1, R, TDat_mix$G)
+    # Parameter: pi_I1
+    pi_I_up <- update_pi_I(lambda, pi_A1, pi_I1, R, TDat_mix$G)
+    # Paramater: lambda
+    lambda_up <- update_lambda(lambda, pi_A1, pi_I1, R, TDat_mix$G)
+    
+    # Now we replace the parameters
+    lambda <- lambda_up
+    pi_A1 <- pi_A_up
+    pi_I1 <- pi_I_up
+    
+    #cat( "Iteration:", v, "| PI A1 =", pi_A1, "| PI I0 =", pi_I0, "| Lambda =", lambda,  "\n" )
   }
 
-  ################################
-  ## 2: Conditional expectation ##
-  ################################
-  
-  # See custom functions and derivations in PDF
+  # Save parameters
+  sim_PIA <- c(sim_PIA, pi_A1)
+  sim_PII <- c(sim_PII, pi_I1)
+  sim_lambda <- c(sim_lambda, lambda)
+}
 
-  #####################################
-  ## 3: Maximise (update parameters) ##
-  #####################################
+true_PIA1
+mean(sim_PIA)
+
+true_PII1
+mean(sim_PII)
+
+true_lambda
+mean(sim_lambda)
+
+
+# Test function for R package
+EM_binom <- function(Y, N, inLam, inPI1, inPI2, max.iter = 250){
   
-  # Parameter: pi_A1
-  pi_A_up <- update_pi_A(lambda, pi_A1, pi_I1, R, TDat_FC)
-  # Parameter: pi_I1
-  pi_I_up <- update_pi_I(lambda, pi_A1, pi_I1, R, TDat_FC)
-  # Paramater: lambda
-  lambda_up <- update_lambda(lambda, pi_A1, pi_I1, R, TDat_FC)
+  ##################
+  # Local functions
+  ##################
   
-  # Now we replace the parameters
-  lambda <- lambda_up
-  pi_A1 <- pi_A_up
-  pi_I1 <- pi_I_up
+  # Define function weights (using current step in algorithm)
+  weight <- function(lambda, pi_1, pi_2, N, Yi){
+    # First calculate numinator: lambda times density of binomial
+    num <- lambda * dbinom(x = Yi, size = N, prob = pi_1)
+    # Now calculate denominator
+    denom <- num + ((1 - lambda) * dbinom(x = Yi, size = N, prob = pi_2))
+    # Return value
+    return(num/denom)
+  }
   
-  cat( "Iteration:", v, "| PI A1 =", pi_A1, "| PI I1 =", pi_I1, "| Lambda =", lambda,  "\n" )
+  # Maximalization function that returns updated
+  #   value for pi_1, given current values (_c)
+  update_pi_1 <- function(lambda_c, pi_1_c, pi_2_c, N, Yi){
+    # Weight of current step
+    weight_c <- weight(lambda_c, pi_1_c, pi_2_c, N, Yi = Yi)
+    # First calculate numinator
+    num <- sum(weight_c * Yi)
+    # Now calculate denominator
+    denom <- sum(weight_c * N)
+    # Return value
+    return(num/denom)
+  }
+  
+  # Same for pi_2
+  update_pi_2 <- function(lambda_c, pi_1_c, pi_2_c, N, Yi){
+    # Weight of current step
+    weight_c <- weight(lambda_c, pi_1_c, pi_2_c, R, Yi = Yi)
+    # First calculate numinator
+    num <- sum(((1 - weight_c) * Yi))
+    # Now calculate denominator
+    denom <- sum(((1 - weight_c) * N))
+    # Return value
+    return(num/denom)
+  }
+  
+  # Same for lambda
+  update_lambda <- function(lambda_c, pi_1_c, pi_2_c, N, Yi){
+    mean(weight(lambda_c, pi_1_c, pi_2_c, N, Yi))
+  }
+  
+  ##################
+  # EM ALGORITHM  #
+  ##################
+  
+  # Start for loop of algorithm steps
+  for(v in 1:max.iter){
+    ################################
+    ## 1: Initial starting values ##
+    ################################
+    if(v == 1){
+      lambda <- inLam
+      pi_1 <- inPI1
+      pi_2 <- inPI2
+    }
+    
+    ################################
+    ## 2: Conditional expectation ##
+    ################################
+    
+    # See custom functions
+    
+    #####################################
+    ## 3: Maximise (update parameters) ##
+    #####################################
+    
+    # Parameter: pi_A1
+    pi_1_up <- update_pi_1(lambda, pi_1, pi_2, N, Y)
+    # Parameter: pi_I1
+    pi_2_up <- update_pi_2(lambda, pi_1, pi_2, N, Y)
+    # Paramater: lambda
+    lambda_up <- update_lambda(lambda, pi_1, pi_2, N, Y)
+    
+    # Now we replace the parameters
+    lambda <- lambda_up
+    pi_1 <- pi_1_up
+    pi_2 <- pi_2_up
+  }
+  
+  # data frame with results
+  res <- data.frame('lambda' = lambda, 'PI1' = pi_1, 'PI2' = pi_2)
+  return(res)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+EM_binom(Y = TDat_mix$G, N = R, inLam = true_lambda, inPI1 = true_PIA1, inPI2 = true_PII1, max.iter = 100)
 
 
 
