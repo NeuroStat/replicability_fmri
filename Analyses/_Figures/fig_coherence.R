2ee5e1522d0334bd78130ec5da9400c8cf8410c7
HBossier <bossier.han@gmail.com>
2018-06-11 17:54:52 +0200

    Adding coherence measure for uncorrected and version of Thirion.
    Measurement of Coherence is now calculated when doing voxelwise uncorrected
    tests at 0.001.
    Furthermore, there was a typo in the paper of Thirion et al. (2007).
    However, this was just a type, it does not seem to be a mathematical error.

---
+++ Analyses/_Figures/fig_coherence.R
@@ -0,0 +1,303 @@
+####################
+#### TITLE:     Plot index of coherence under increasing sample size.
+#### Contents: 	
+#### 
+#### Source Files: /FreddieFreeloader/Analyses/_Figures
+#### First Modified: 31/05/2018
+#### Notes: 
+#################
+
+
+
+##
+###############
+### Notes
+###############
+##
+
+# Data analysis:
+# Make independent batches of subjects.
+# Each run, we estimate the mixture of two binomials on the summed thresholded maps within a sample size.
+
+# Here we read in the processed data, calculate the coherence using Cohen's Kappa and plot the results.
+# We do this for each sample size and each run.
+
+
+
+##
+###############
+### Preparation
+###############
+##
+
+# Location of intermediate results
+LocIntRes <- '../_IntData/'
+
+# Load in libraries
+library(tidyverse)
+library(magrittr)
+library(cowplot)
+library(lattice)
+library(gridExtra)
+library(oro.nifti)
+library(reshape2)
+library(RColorBrewer)
+library(Hmisc)
+library(NeuRRoStat)
+
+# Function described by Thirion et al. (2007) to calculate Cohen's Kappa
+ThirionKappa <- function(lambda, piA1, piI1){
+  # Check if the values are within 0 and 1
+  if(any(c(lambda, piA1, piI1) < 0 | c(lambda, piA1, piI1) > 1)) error('Values should be between 0 and 1!')
+  
+  # Now calculate the piA0 and piI0
+  piA0 <- 1 - piA1
+  piI0 <- 1 - piI1
+  
+  # Calculate the proportion of correct classifications (p0)
+  p0 <- lambda*piA1 + (1 - lambda) * piI0
+  
+  ## Agreement by chance
+  # pi1 or also denoted as tau
+  pi0 <- lambda * piA0 + (1 - lambda) * piI0
+  # pi1 or also given by 1 - tau
+  pi1 <- 1 - pi0
+  # pC
+  pC <- lambda * pi0 + (1 - lambda) * pi1
+  
+  # Cohen's Kappa, defined as Thirion
+  CohKap_Thi <- (p0 - pC)/(1 - pC)
+  
+  return(CohKap_Thi)
+}
+
+# Dimension of the brains
+DIM <- c(53,63,46)
+
+# Number of runs
+NRUNS <- 50
+
+# Starting amount of subjects in group analysis
+startingTotal <- 10
+
+# Database total
+DATTOTAL <- 1400
+
+# Total amount of possible subjects if we have a maximum of 3 disjoint subsets
+NTOT <- 460
+
+# Steps in the sequence (sample sizes)
+steps <- seq(startingTotal, NTOT, by = 10)
+
+# Create the sequence
+sequence <- data.frame()
+for(k in 1:NRUNS){
+  for(i in 1:length(steps)){
+    # Number of disjoint sets in this sample size
+    numDS <- floor(DATTOTAL/steps[i])
+    for(s in 1:numDS){
+      sequence <- rbind(sequence,
+                        data.frame('run' = k, 'step' = i, 'set' = s))
+    }
+  }
+}
+
+# Dimension of this data frame
+dimSeq <- dim(sequence)[1]
+
+# Variables for plotting
+subjBreak <- c(seq(10,110,by=20), seq(150,NTOT, by=50))
+
+# Possible thresholding scenario (uncorrected or fdr)
+scenario_pos <- c('unc', 'fdr')
+#scenario <- scenario_pos[1]
+
+##
+###############
+### Read in data
+###############
+##
+
+# Data frame with estimated parameters using EM: voxelwise FDR and uncorrected
+EMParam <- data.frame() %>% as_tibble()
+for(r in 1:length(scenario_pos)){
+  EMParam <-
+    readRDS(paste0(LocIntRes, 'EM_params_', scenario_pos[r],'.rda')) %>% 
+      mutate(threshold = scenario_pos[r]) %>%
+    bind_rows(EMParam, .)
+}
+
+# Add sequence information
+EMParam$sequence <- factor(rep(rep(1:(dim(EMParam)[1]/4), each = 2), 2))
+
+##
+###############
+### Check results of EM: FDR only
+###############
+##
+
+# Let us plot the amount of iterations needed to estimate the parameters
+EMParam %>% filter(final == TRUE) %>%
+  filter(threshold == 'fdr') %>%
+  ggplot(., aes(sequence, num.iter)) +
+  geom_col()
+# Not very informative...
+
+# Better have some kind of barplot
+EMParam %>% filter(threshold == 'fdr') %>%
+  ggplot(., aes(num.iter, group = final)) +
+    geom_bar(aes(fill = final), position = 'dodge2') +
+    scale_y_continuous("Count over all steps * runs") +
+    scale_x_continuous('Number of iterations') +
+    ggtitle('Number of iterations needed in the two-step EM algorithm',
+            subtitle = 'Split according to first run or final estimation.') +
+    theme_classic()
+ggsave(filename = paste(getwd(), '/EM_Checks/iterations_EM.png', sep = ''), plot = last_plot())
+
+# Difference in coefficients
+# Values of first run
+valF <- EMParam %>% filter(final == FALSE) %>% filter(threshold == 'fdr') %>%
+  select(lambda, PI1, PI2)
+# Values of second run
+valS <- EMParam %>% filter(final == TRUE) %>% filter(threshold == 'fdr') %>%
+  select(lambda, PI1, PI2)
+
+# Plot difference
+plotRuns <- seq(1,NRUNS,by = 6)
+for(r in 1:length(plotRuns)){
+  #print(plotRuns[r] : (plotRuns[r] + 5))
+  ToPrint <- plotRuns[r] : (plotRuns[r] + 5)
+  if(max(ToPrint) > 50){
+    ToPrint <- min(ToPrint):50
+  }
+  ToPlot <- data.frame(valF - valS, step = EMParam$step, run = EMParam$run) %>% 
+    filter(run %in% ToPrint) %>%
+    gather(., key = 'parameter', value = 'value', 1:3) %>%
+    ggplot(., aes(x = step, y = value, group = parameter)) +
+    geom_line(aes(colour = parameter), size = 0.7) +
+    facet_wrap( ~ run) +
+    ggtitle(paste0('Differences in parameter estimates between first and second attempt
+                   in the two step EM algorithm'))
+  ggsave(filename = paste(getwd(), '/EM_Checks/DiffEM_part', r, '.png', sep = ''), plot = ToPlot)
+}
+
+# Seems ok! Go with final estimates.
+
+##
+###############
+### Calculate Cohen's Kappa: FDR
+###############
+##
+
+# Filter the final estimates
+Kappa <- EMParam %>% filter(final == TRUE) %>% filter(threshold == 'fdr') %>%
+  # Remove columns that don't provide information
+  select(-num.iter, -final, -sequence) %>%
+  # Transform step to sample size
+  mutate(SampleSize = step * 10) %>%
+  # Calculate Kappa
+  mutate(kappa = NeuRRoStat::CohenKappa(lambda = lambda, piA1 = PI1, piI1 = PI2)) %>%
+  # Put NaN to 0
+  mutate(kappa = ifelse(is.nan(kappa), 0, kappa)) %>%
+  # Remove columns
+  select(run, SampleSize, kappa)
+
+# Average, min and max per sample size
+Kappa %>% group_by(SampleSize) %>%
+  summarise(AvgKap = mean(kappa),
+            minKap = min(kappa),
+            maxKap = max(kappa)) 
+
+
+# Plot results
+ggplot(Kappa, aes(x = SampleSize, y = kappa)) +
+geom_point() +
+geom_smooth()
+
+# Using boxplots
+ggplot(Kappa, aes(x=factor(SampleSize), y = kappa)) + 
+  geom_boxplot(outlier.size = .7, outlier.color = 'orange', size = 0.3) +
+  scale_x_discrete(breaks = subjBreak, name = "Sample size") +
+  scale_y_continuous(name='Kappa', breaks = seq(0,1,0.2)) +
+  labs(caption = 'FDR = 0.05') +
+  theme_bw()
+
+
+# Version OHBM 2018
+subjBreak <- c(seq(10,110,by=30), seq(150,700, by=50))
+ggplot(Kappa, aes(x=factor(SampleSize), y = kappa)) + 
+  geom_boxplot(outlier.size = .7, outlier.color = 'orange', size = 0.3) +
+  scale_x_discrete(breaks = subjBreak, name = "Sample size") +
+  scale_y_continuous(name=expression(Coherence~~(kappa)), breaks = seq(0,1,0.2)) +
+  labs(subtitle = 'FDR = 0.05') +
+  theme_classic() +
+  theme(panel.grid.major = element_line(size = 0.8),
+        panel.grid.minor = element_line(size = 0.8),
+        axis.title.x = element_text(face = 'plain'),
+        axis.title.y = element_text(face = 'plain'),
+        axis.text = element_text(size = 11, face = 'plain'),
+        axis.ticks = element_line(size = 1.3),
+        axis.ticks.length=unit(.20, "cm"),
+        axis.line = element_line(size = .75),
+        title = element_text(face = 'plain'),
+        plot.title = element_text(hjust = 0.5),
+        legend.position = 'top')
+
+
+
+##
+###############
+### Calculate Cohen's Kappa: comparing uncorrected, FDR and the one from Thirion
+###############
+##
+
+# First calculate Kappa as written in text of Thirion et al. (2007)
+# Filter the final estimates
+Kappa_both <- EMParam %>% filter(final == TRUE) %>% 
+  # Remove columns that don't provide information
+  select(-num.iter, -final, -sequence) %>%
+  # Transform step to sample size
+  mutate(SampleSize = step * 10) %>%
+  # Calculate Kappa
+  mutate(kappa = NeuRRoStat::CohenKappa(lambda = lambda, piA1 = PI1, piI1 = PI2)) %>%
+  # Calculate Kappa according to Thirion
+  mutate(kappa_Thirion = ThirionKappa(lambda = lambda, piA1 = PI1, piI1 = PI2)) %>%
+  # Put NaN to 0
+  mutate(kappa = ifelse(is.nan(kappa), 0, kappa)) %>%
+  # Remove columns
+  select(run, SampleSize, kappa, kappa_Thirion, threshold) %>%
+  # gather results
+  gather(key = 'typeOfkappa', value = 'value', 3:4)
+
+
+# Plot the results
+ggplot(Kappa_both, aes(x=factor(SampleSize), y = value, fill = threshold)) + 
+  geom_boxplot(aes(x = factor(SampleSize), fill = threshold), outlier.size = .4, outlier.color = 'orange', size = 0.3) +
+  scale_x_discrete(breaks = subjBreak, name = "Sample size") +
+  scale_y_continuous(name='Kappa', breaks = seq(0,1,0.2)) +
+  scale_fill_brewer('Threshold', labels = c('FDR 0.05', 'Uncorrected 0.001'), 
+                    type = 'qual') +
+  facet_grid(typeOfkappa ~ .) +
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+
+# Figure seems not good. Probably the text contained an error, but not the caluclation.
+
+##
+###############
+### Calculate Cohen's Kappa: comparing uncorrected with FDR
+###############
+##
+
+Kappa_both %>% 
+  filter(typeOfkappa == 'kappa') %>%
+  ggplot(., aes(x=factor(SampleSize), y = value, fill = threshold)) + 
+    geom_boxplot(aes(x = factor(SampleSize), fill = threshold), outlier.size = .4, outlier.color = 'orange', size = 0.3) +
+    scale_x_discrete(breaks = subjBreak, name = "Sample size") +
+    scale_y_continuous(name='Kappa', breaks = seq(0,1,0.2)) +
+    scale_fill_brewer('Threshold', labels = c('FDR 0.05', 'Uncorrected 0.001'), 
+                      type = 'qual', palette = 2) +
+    theme_bw() +
+    theme(legend.position = 'bottom')
+
---
+++ Analyses/_PreProcessing/PreProcCoher.R
@@ -0,0 +1,195 @@
+####################
+#### TITLE:     Calculate coherence, after fitting a mixture of binomials using EM.
+#### Contents: 	
+#### 
+#### Source Files: /FreddieFreeloader/Analyses/_PreProcessing/_Coherence
+#### First Modified: 29/05/2018
+#### Notes: 
+#################
+
+
+
+##
+###############
+### Notes
+###############
+##
+
+# Data analysis:
+# Make independent batches of subjects.
+# Each run, we estimate the mixture of two binomials on the summed thresholded maps within a sample size.
+
+# Here we read in the summed maps and then estimate the mixture using an EM algorithm.
+
+
+
+##
+###############
+### Preparation
+###############
+##
+
+# Possible thresholding scenario
+scenario_pos <- c('uncorrected', 'fdr')
+scenario <- scenario_pos[1]
+
+# Location of raw data: not included in Github folder (too large)
+RawDat <- ifelse(scenario == 'uncorrected',
+  "/Volumes/2_TB_WD_Elements_10B8_Han/PhD/IMAGENDATA/Data/FreddieFreeloader/Data/Coherence/Results_Unc/Results",
+  "/Volumes/2_TB_WD_Elements_10B8_Han/PhD/IMAGENDATA/Data/FreddieFreeloader/Data/Coherence/Results_FDR/Results")
+  
+
+# Save intermediate results
+SaveLoc <- '/Volumes/2_TB_WD_Elements_10B8_Han/PhD/IMAGENDATA/Data/FreddieFreeloader/Script.git/FreddieFreeloader/Analyses/_IntData'
+
+# Load in libraries
+library(tidyverse)
+library(magrittr)
+library(lattice)
+library(gridExtra)
+library(oro.nifti)
+library(reshape2)
+library(RColorBrewer)
+library(Hmisc)
+library(NeuRRoStat)
+
+#####################
+#### Custom functions
+#####################
+
+# Function to create new values near given values, with restriction of the 
+# updated values not going below 0 or above 1 (used in two-step EM).
+nearVal <- function(values){
+  add <- round(values, 4) + 0.025
+  if(any(add <= 0 | add >= 1)){
+    return(values)
+  }else{
+    return(add)
+  }
+}
+
+
+# Dimension of the brains
+DIM <- c(53,63,46)
+
+# Number of runs
+NRUNS <- 50
+
+# Starting amount of subjects in group analysis
+startingTotal <- 10
+
+# Database total
+DATTOTAL <- 1400
+
+# Total amount of possible subjects if we have a maximum of 3 disjoint subsets
+NTOT <- 460
+
+# Steps in the sequence (sample sizes)
+steps <- seq(startingTotal, NTOT, by = 10)
+
+# Create the sequence
+sequence <- data.frame()
+for(k in 1:NRUNS){
+  for(i in 1:length(steps)){
+    # Number of disjoint sets in this sample size
+    numDS <- floor(DATTOTAL/steps[i])
+    for(s in 1:numDS){
+      sequence <- rbind(sequence,
+                        data.frame('run' = k, 'step' = i, 'set' = s))
+    }
+  }
+}
+
+# Dimension of this data frame
+dimSeq <- dim(sequence)[1]
+
+# Initial guess for starting values in the EM algorithm
+# We want to estimate 
+#     lambda: the proportion of truly active voxels, 
+#     PI_A1: the probability of a truly active voxel being declared significant
+#     PI_I1: the probability of a truly inactive voxel being incorrectly declared significant
+ini_lambda <- 0.1
+ini_PI_A1 <- 0.6
+ini_PI_I1 <- 0.05
+
+##
+###############
+### Estimation
+###############
+##
+
+# We want to estimate 
+#     lambda (the proportion of truly active voxels), 
+#     PI_A1 (the probability of a voxel being truly active)
+# and PI_I1 (the probability of a voxel being incorrectly activated)
+# These parameters will then be used to calculate Cohen's kappa. 
+
+# We do this for each run and for each sample size (step)
+
+# Empty data frame
+EM_params <- data.frame() %>% as_tibble()
+
+# Start for loop over all runs 
+for(k in 1:NRUNS){
+  print(paste0('At run ', k))
+  for(i in 1:length(steps)){
+    # Number of disjoint sets in this sample size
+    numDS <- floor(DATTOTAL/steps[i])
+    
+    # Read in the summed map and its mask
+    sumMap <- readNIfTI(paste(RawDat, '/Run_', k, '/Step_', i, '/Gmap.nii.gz', sep = ''))[,,]
+    maskStep <- readNIfTI(paste(RawDat, '/Run_', k, '/Step_', i, '/Mask_Gmap.nii.gz', sep = ''))[,,]
+    
+    # Put masked values to NA
+    sumMap[maskStep == 0] <- NA
+    
+    # Go to vector and remove NA
+    sumVec <- c(array(sumMap, dim = prod(DIM)))
+    sumVec_val <- sumVec[!is.na(sumVec)]
+    
+    # Initiate EM algorithm (see NeuRRoStat package)
+    # We first run the algorithm with sensible starting values for fMRI data.
+    # Then we run it with values close to those obtained as a starting value.
+    # CAREFUL: N IS NOT THE SAMPLE SIZE, BUT THE NUMBER OF DISJOINT SETS!!!
+    iniGuess <- NeuRRoStat::EMbinom(Y = sumVec_val, N = numDS, iniL = ini_lambda,
+                        iniPI1 = ini_PI_A1, iniPI2 = ini_PI_I1)
+    nearValues <- nearVal(iniGuess[,-4])
+    if(nearValues$PI1 == nearValues$PI2){
+      EM_param <- iniGuess
+    }else{
+      EM_param <- NeuRRoStat::EMbinom(Y = sumVec_val, N = numDS, 
+                          iniL = nearValues$lambda,
+                          iniPI1 = nearValues$PI1, iniPI2 = nearValues$PI2)
+    }
+    
+    # Save the parameters (of initial and final estimation) with info about step
+    EM_params <- bind_rows(EM_params,
+            data.frame(iniGuess, 'step' = i, 'run' = k, 'final' = FALSE),
+            data.frame(EM_param, 'step' = i, 'run' = k, 'final' = TRUE))
+    
+    # Reset parameters
+    rm(iniGuess, EM_param, sumVec_val, sumVec, sumMap, maskStep)
+  }
+}
+
+
+##
+###############
+### Save processed data
+###############
+##
+
+# ID for EM_params
+if(scenario == 'uncorrected'){
+  saveRDS(EM_params, file = paste(SaveLoc, '/EM_params_unc.rda', sep = ''))  
+}
+if(scenario == 'fdr'){
+  saveRDS(EM_params, file = paste(SaveLoc, '/EM_params_fdr.rda', sep = ''))  
+}
+
+
+
+
+
+
+
